a47b3c462510c9ff5223443600356a610b6d567e
akari <myfrankboy@gmail.com>
2022-02-18 19:43:26 +0900

    index表示

---
+++ app/Http/Controllers/ListingsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Auth;
+use Validator;
+
+use Illuminate\Http\Request;
+
+class ListingsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $listings = Listing::where('user_id', Auth::user()->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+            
+            return view('listing/index', ['listings' => $listings]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/','App\Http\Controllers\ListingsController@index');
+
 
 Auth::routes();
 
