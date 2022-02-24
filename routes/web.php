0dea72bdbb8dfda1bca285a58354e24e94d0d32a
akari <myfrankboy@gmail.com>
2022-02-24 15:31:23 +0900

    cardsコントローラー作成

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,21 @@ Route::get('/listingsdelete/{listing_id}', 'App\Http\Controllers\ListingsControl
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//カード新規画面
+Route::get('listing/{listing_id}/card/new', 'App\Http\Controllers\CardsController@new')->name('new_card');
+
+//カード新規処理
+Route::post('/listing/{listing_id}/card', 'App\Http\Controllers\CardsController@store');
+
+//カード詳細画面
+Route::get('listing/{listing_id}/card/{card_id}', 'App\Http\Controllers\CardsController@show');
+
+//カード編集画面
+Route::get('listing/{listing_id}/card/{card_id}/edit', 'App\Http\Controllers\CardsController@edit');
+
+//カード更新処理
+Route::post('/card/edit', 'App\Http\Controllers\CardsController@update');
+
+//カード削除処理
+Route::get('listing/{listing_id}/card/{card_id}/delete', 'App\Http\Controllers\CardsController@destroy');
